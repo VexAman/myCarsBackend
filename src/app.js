df140c149f5e9e378dc6954920d7d17d230ec2e4
VexAman <amanchouhan292325@gmail.com>
2024-11-18 23:48:58 +0530

    added a default route

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
+app.get("/", (req, res) => {
+    res.send("aman");
+  });
 //routes import
 import userRouter from './routes/user.routes.js'
 import carRouter from './routes/car.routes.js'
